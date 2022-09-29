b5bba0b08e426e9de8fa60a3be82562d8b4509ee
Rodrigo Seibel <rodrigosoparajogos@hotmail.com>
2022-09-29 20:05:25 -0300

    add exercicio elemento

---
+++ fundamentos/bloco-5-dom-e-seletores/dia-2-elements/buscar-elementos/script.js
@@ -0,0 +1,27 @@
+// 1-Acessar  elementoondevoceesta, acessar pai e trocar a cor
+
+// console.log(document.getElementById('elementoOndeVoceEsta').parentNode.style.background = 'blue'  )  ;
+ 
+// 2-Acessar primeirofilhodofilho e adicionar texto
+
+//document.getElementById('primeiroFilhoDoFilho').innerText = 'Teste de texto';
+
+// 3-Acessar primeiroFilho, apartir de filho
+
+//console.log(document.getElementById('pai').firstElementChild );
+
+// 4 -Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+
+//console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling);
+
+// 5 - Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+
+//console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
+
+// 6- Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+
+//console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling );
+
+//Agora acesse o terceiroFilho a partir de pai.
+
+console.log(document.getElementById('pai').lastElementChild.previousElementSibling);
