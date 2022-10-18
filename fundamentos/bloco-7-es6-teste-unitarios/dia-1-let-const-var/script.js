7208ecd714e1fe737e31f7372337020912f1e4d1
Rodrigo Seibel <rodrigosoparajogos@hotmail.com>
2022-10-18 14:56:44 -0300

    add

--- fundamentos/bloco-7-es6-teste-unitarios/dia-1-let-const-var/script.js
+++ fundamentos/bloco-7-es6-teste-unitarios/dia-1-let-const-var/script.js
@@ -1,13 +1,21 @@
-const testingScope =  (escopo) => {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = `${ifScope}  ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-      console.log(elseScope);
-    }
+// Exercicio Principal
+
+ const testingScope =  (escopo) => {
+     if (escopo === true) {
+       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+       ifScope = `${ifScope}  ótimo, fui utilizada no escopo !`;
+       console.log(ifScope);
+     } else {
+       const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+     console.log(elseScope);
+     }
     
   }
 
-  testingScope(true);
+   testingScope(true);
+
+
+
+
+
+
