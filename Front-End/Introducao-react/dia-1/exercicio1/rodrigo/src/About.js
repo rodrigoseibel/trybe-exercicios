b3f72900c6ea39b9f74bd7b7aa82fa226d0da778
Rodrigo Seibel <rodrigosoparajogos@hotmail.com>
2022-11-24 10:05:48 -0300

    add

---
+++ Front-End/Introducao-react/dia-1/exercicio1/rodrigo/src/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Rodrigo
+        </h1>
+        <p>
+          26 anos, aprendendo e tomando surra
+          s
+        </p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>                    
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
